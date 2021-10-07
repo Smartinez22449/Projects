407e9c143a4cafb8c4b073922e9f5469f5f9af76
Smartinez22449 <89529592+Smartinez22449@users.noreply.github.com>
2021-10-06 22:05:48 -0400

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,12 @@
+public class Add{
+    private double num2;
+    private double num1;
+    
+    
+    public Add(int pNum1, int num){
+       
+        
+
+
+    }    
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println("Enter operator.");
+        String operator = input.nextLine();
+        System.out.println("Enter first number.");
+        double num1 = input.nextDouble();
+        System.out.println("Enter second number.");
+        double num2 = input.nextDouble();
+        
+    }
+}
